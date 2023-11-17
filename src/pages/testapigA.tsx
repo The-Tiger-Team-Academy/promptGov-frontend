2935c53cf9a6185d7a329fe703d407a1dc3ce03e
Best2546 <125348454+Bestbest2546@users.noreply.github.com>
2023-11-17 19:33:38 +0000

    update

--- src/pages/testapigA.tsx
+++ src/pages/testapigA.tsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 
-const MyComponent: React.FC = () => {
+const createDocuments: React.FC = () => {
     const [inputData, setInputData] = useState('');
     const [logoFile, setLogoFile] = useState<File | null>(null);
+    const [fileName, setFileName] = useState('Upload Logo');
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+    // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     if (event.target.files) {
+    //         setSelectedFile(event.target.files[0]);
+    //     }
+    // };
+
+    // const handleClick = () => {
+    //     fileInputRef.current?.click();
+    // };
 
     const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();  // Prevent default form submission behavior
+        event.preventDefault();
 
         const formData = new FormData();
         if (logoFile) {
-            formData.append('logo', logoFile); // แนบไฟล์รูปภาพ (หากมี)
+            formData.append('logo', logoFile);
         }
         try {
             // Replace with your API endpoint
@@ -22,18 +37,15 @@ const MyComponent: React.FC = () => {
                 responseType: 'blob',
             });
 
-            // Create a URL for the blob
+
             const fileURL = window.URL.createObjectURL(new Blob([response.data]));
-            // Create a temporary link element
             const fileLink = document.createElement('a');
             fileLink.href = fileURL;
-            fileLink.setAttribute('download', 'generated.docx');  // Set the file name for download
+            fileLink.setAttribute('download', 'generated.docx');
             document.body.appendChild(fileLink);
 
-            // Programmatically click the link to trigger the download
             fileLink.click();
 
-            // Clean up: remove the link and revoke the object URL
             fileLink.remove();
             window.URL.revokeObjectURL(fileURL);
         } catch (error) {
@@ -42,33 +54,47 @@ const MyComponent: React.FC = () => {
     };
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
-            setLogoFile(event.target.files[0]); // เซ็ตไฟล์รูปภาพเมื่อมีการเลือก
+            setLogoFile(event.target.files[0]);
         }
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Enter Data:
-                    <input
-                        type="text"
-                        value={inputData}
-                        onChange={(e) => setInputData(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Upload Logo:
-                    <input
-                        type="file"
-                        accept="image/png"
-                        onChange={handleFileChange} // เมื่อมีการเลือกไฟล์
-                    />
-                </label>
-                <button type="submit">SUBMIT</button>
-            </form>
-        </div>
+        <Grid container spacing={0}>
+            <Grid item xs={12}>
+                <div style={{ display: 'flex', width: '100%', flexDirection: 'row', alignItems: "center", justifyContent: 'center', border: '1px solid black' }}>
+                    <label>
+                        {/* Input Data: */}
+                        <input
+                            type="text"
+                            value={inputData}
+                            onChange={(e) => setInputData(e.target.value)}
+                            style={{ borderRadius: '10px', height: '50px' }}
+                            placeholder='Input data'
+                        />
+                    </label>
+                </div>
+            </Grid>
+            <Grid item xs={12}>
+                <div style={{ display: 'flex', width: '100%', flexDirection: 'row', alignItems: "center", justifyContent: 'center', border: '1px solid black' }}>
+                    <label>
+                        {/* Upload Logo: */}
+                        <input
+                            type="file"
+                            // accept="image/png"
+                            onChange={handleFileChange}
+                        />
+                    </label>
+                    <button type="submit" onClick={handleSubmit} style={{ borderRadius: '5px', height: '35px', backgroundColor: 'pink' }}>SUBMIT</button>
+                </div>
+
+            </Grid>
+            {/* <Grid item xs={12}>
+                <div style={{ display: 'flex', width: '100%', flexDirection: 'row', alignItems: "center", justifyContent: 'center', border: '1px solid black' }}>
+                    <button type="submit" onClick={handleSubmit}>SUBMIT</button>
+                </div>
+            </Grid> */}
+        </Grid>
     );
 };
 
-export default MyComponent;
+export default createDocuments;
