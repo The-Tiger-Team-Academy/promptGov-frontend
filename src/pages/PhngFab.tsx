01e788773f8700eb451bf862f8373324316c825f
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-11-21 12:35:16 +0000

    fix build

--- src/pages/PhngFab.tsx
+++ src/pages/PhngFab.tsx
@@ -1,10 +1,6 @@
 import { Container, Grid } from "@mui/material"
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+
 
 const PhngFab = () => {
     return (
@@ -60,5 +56,6 @@ const PhngFab = () => {
                 </Container>
             </Grid>
         </Grid>
+    )}
 
 export default PhngFab
