d350f7ee84511acfa95c35d2f4f6edee661cc854
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-11-21 12:50:15 +0000

    add payments with stripe

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -1,9 +1,23 @@
+import { Container } from "@mui/material"
+import PaymentHook from "./hooks/payments.hook";
 
 
 
 const PaymentPage = () => {
+    const { selectedPrize, handlePrizeClick} = PaymentHook();
     return (
-        <div>this is payment page</div>
+        <div>this is payment page
+            <Container>
+                <button
+                    onClick={() =>
+                        handlePrizeClick("price_1OEtCBAom1IgIvKKjrrwjZN3")
+                    }
+                >
+                    จ่ายเงิน
+                </button>
+            </Container>
+
+        </div>
     )
 
 }
