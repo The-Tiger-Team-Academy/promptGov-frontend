b6ea90f5800a282029c5247528a6cca016ee78df
meixxx <91084715+meixxx@users.noreply.github.com>
2023-11-28 08:04:47 +0000

    update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,10 +3,24 @@ import { Roboto } from "next/font/google";
 // import styles from "./login.module.css";
 import Image from "next/image";
 import Button from "@mui/material/Button";
+import { Container, Typography } from "@mui/material";
+import { LocaleRouteNormalizer } from "next/dist/server/future/normalizers/locale-route-normalizer";
 import signInWithGoogle from "@/module/payment/services/signInWithGoogle";
 import { NextRouter, useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+const defaultTheme = createTheme();
 
 export default function LoginPage() {
   const [name, setName] = useState<string>('');
@@ -32,11 +46,10 @@ export default function LoginPage() {
   const login = async () => {
     try {
       const result = await signInWithGoogle();
-      if (result) {
-        // const user: IUser = result.user;
-        setName(result.user.displayName);
-        setEmail(result.user.email);
-        setImage(result.user.photoURL);
+      if (result && result.user) {
+        console.log(result.user.email);
+        router.push('./payment')
+        alert("Login success")
       } else {
         console.log("No user data available");
       }
@@ -45,65 +58,34 @@ export default function LoginPage() {
     }
   };
 
-  useEffect(() => {
-    if (name && email && img) {
-      postData();
-    }
-  }, [name, email, img]);
+
 
   return (
-    <div>
-      <div
-        style={{
-          zIndex: -1,
-          position: "fixed",
-          width: "60vw",
-          height: "60vh",
-          right: "50%",
-          top: "62%",
-        }}
-      >
-        <Image
-          src="/img/runs_on_paper.jpg"
-          alt=""
-          objectFit="cover"
-          layout="fill"
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh', backgroundColor: '#fefefe' }}>
+        <CssBaseline />
+        <Grid
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://i.postimg.cc/8cBYYj79/runs-on-paper.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: '85%',
+            backgroundPosition: '-50% 100%',
+          }}
         />
-      </div>
-
-      <div
-        style={{
-          zIndex: -1,
-          position: "fixed",
-          width: "35vw",
-          height: "53vh",
-          bottom: "70%",
-          left: "128%",
-        }}
-      >
-        <Image src="/img/Vector.png" alt="" layout="fill" objectFit="cover" />
-      </div>
-      <div
-        style={{
-          zIndex: -1,
-          position: "fixed",
-          width: "15vw",
-          height: "53vh",
-          top: "-40%",
-          left: "7%",
-        }}
-      >
-        <Image src="/img/Vector_logo.png" alt="" width={62} height={63} />
-      </div>
-
-      <div>
-        <main></main>
-        <div>Welcome!</div>
-        <div>Lorem ipsum dolor sit amet consectetur.</div>
-        <Button variant="contained" onClick={() => login()}>
-          Sign in with Google
-        </Button>
-      </div>
-    </div>
+        <Grid
+          xs={12}
+          sm={8}
+          md={5}
+          sx={{
+            backgroundImage: 'url(https://i.postimg.cc/50dT7rBk/Vector.png)',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: '60%',
+            backgroundPosition: '125% -20%',
+          }} />
+      </Grid>
+    </ThemeProvider>
   );
 }
