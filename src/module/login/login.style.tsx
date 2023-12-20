790685be3acf811f27f9a448078b0e10954b0584
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-12-20 16:25:43 +0000

    refactor

--- src/module/login/login.style.tsx
+++ src/module/login/login.style.tsx
@@ -3,90 +3,107 @@ import { useTheme } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
 
 
-const boxContainerStyles = (isSmallScreen: boolean): SxProps => ({
-  backgroundImage: 'url(https://i.postimg.cc/rsvTfqNR/runs-on-paper-a.png)',
-  backgroundPosition: 'bottom left',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: isSmallScreen ? 'cover' : 'auto',
-  minHeight: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  position: 'relative',
-});
 
-const gridItemStyles = (isSmallScreen: boolean, imageStyles: any): SxProps => ({
-  backgroundImage: 'url(https://i.postimg.cc/mkTCtGHM/Vector.png)',
-  backgroundPosition: 'top right',
-  backgroundSize: isSmallScreen ? 'cover' : 'auto',
-  ...imageStyles,
-  backgroundRepeat: 'no-repeat',
-  position: 'absolute',
-  top: 0,
-  right: 0,
-  zIndex: 1,
-});
+const useLoginStyles = () => {
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
+    const boxContainer = (isSmallScreen: boolean): SxProps => ({
+        backgroundImage: 'url(https://i.postimg.cc/rsvTfqNR/runs-on-paper-a.png)',
+        backgroundPosition: 'bottom left',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: isSmallScreen ? 'cover' : 'auto',
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        [theme.breakpoints.down('sm')]: {
+            backgroundImage: 'none',
+        },
+    });
 
-const imageStyles = (isSmallScreen: boolean, imageStyles: any): SxProps => ({
-  width: isSmallScreen ? '80vw' : '392px',
-  height: isSmallScreen ? 'auto' : '367px',
-  flexShrink: 0,
-});
+    const gridItem = (isSmallScreen: boolean, imageStyles: any): SxProps => ({
+        backgroundImage: 'url(https://i.postimg.cc/mkTCtGHM/Vector.png)',
+        backgroundPosition: 'top right',
+        backgroundSize: isSmallScreen ? 'cover' : 'auto',
+        ...imageStyles,
+        backgroundRepeat: 'no-repeat',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        zIndex: 1,
+        [theme.breakpoints.down('sm')]: {
+            backgroundImage: 'none',
+        },
+    });
 
+    const imageStyles = {
+        width: isSmallScreen ? '80vw' : '392px',
+        height: isSmallScreen ? 'auto' : '367px',
+        flexShrink: 0,
+      };
 
-const innerBoxStyles: SxProps = {
-  backgroundColor: '#fff',
-  padding: 0,
-  borderRadius: 8,
-  boxShadow: 0,
-  zIndex: 2,
-  mt: 2, // You can directly specify the value here instead of using isSmallScreen ? 2 : 0,
-  marginBottom: '15rem',
+    return { boxContainer, gridItem, imageStyles,isSmallScreen };
 };
 
-const logoBoxStyles: SxProps = {
-  backgroundImage: 'url(https://i.postimg.cc/k41vFCpP/Logo-Prompt-Gov.png)',
-  backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
-  position: 'relative',
-  width: '23rem',
-  height: '5rem',
-  zIndex: 1,
-  backgroundSize: '85%',
+export default useLoginStyles;
+
+
+export const innerBox: SxProps = {
+    backgroundColor: 'transparent',
+    padding: 0,
+    borderRadius: 8,
+    boxShadow: 0,
+    zIndex: 2,
+    mt: 2, // You can directly specify the value here instead of using isSmallScreen ? 2 : 0,
+    marginBottom: '15rem',
+
 };
 
-const typographyStyles: SxProps = {
-  fontFamily: 'Kanit, sans-serif',
-  textAlign: 'center',
-  marginTop: '-0.5rem',
+export const logoBox: SxProps = {
+    backgroundImage: 'url(https://i.postimg.cc/k41vFCpP/Logo-Prompt-Gov.png)',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    position: 'relative',
+    width: '23rem',
+    height: '5rem',
+    zIndex: 1,
+    backgroundSize: '85%',
 };
 
-const buttonStyles: SxProps = {
-  backgroundColor: '#4285F4',
-  borderRadius: 25,
-  width: '16rem',
-  height: '3rem',
-  color: 'white',
-  '&:hover': {
-    backgroundColor: '#c1351a',
-  },
+export const typography: SxProps = {
+    fontFamily: 'Kanit, sans-serif',
+    textAlign: 'center',
+    marginTop: '-0.5rem',
 };
 
-const loginFormStyles: SxProps = {
-  padding: '20px',
-  backgroundColor: 'transparent',
+export const button: SxProps = {
+    backgroundColor: '#4285F4',
+    borderRadius: 25,
+    width: '16rem',
+    height: '3rem',
+    color: 'white',
+    '&:hover': {
+        backgroundColor: '#c1351a',
+    },
+
 };
 
-const LoginStylePage = {
-  boxContainerStyles,
-  gridItemStyles,
-  imageStyles,
-  innerBoxStyles,
-  logoBoxStyles,
-  typographyStyles,
-  buttonStyles,
-  loginFormStyles,
+export const loginForm: SxProps = {
+    padding: '20px',
+    backgroundColor: 'transparent',
+
+};
+
+export const loginStylePage = {
+    innerBox,
+    logoBox,
+    typography,
+    button,
+    loginForm
 }
 
-export default LoginStylePage;
+
+
+
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,8 +5,8 @@ import { Box, Grid, Paper, Button } from '@mui/material';
 import GoogleIcon from '@mui/icons-material/Google';
 import 'animate.css';
 import loginHook from '@/module/login/hook/login.hook';
-import useLoginStyles from "./login.style";
 import LoginStylePage from "@/module/login/login.style";
+import useLoginStyles from "@/module/login/login.style";
 
 
 
@@ -23,16 +23,16 @@ export default function LoginPage() {
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Box sx={gridItem(isSmallScreen, imageStyles)} />
-            <Box sx={style.innerBoxStyles}>
-              <Paper elevation={0} sx={style.loginFormStyles}  >
+            <Box>
+              <Paper elevation={0}>
                 <Grid container direction="column" alignItems="center" spacing={2} >
                   <Grid item >
                     <Box
                       className="animate__animated animate__fadeInRight"
-                      sx={style.logoBoxStyles} />
+                      />
                   </Grid>
                   <Grid item>
-                    <Typography variant='inherit' sx={style.typographyStyles}>เว็บที่ช่วยให้คุณสร้างเอกสารราชการได้ง่ายๆ เพียงไม่กี่คลิก
+                    <Typography variant='inherit'>เว็บที่ช่วยให้คุณสร้างเอกสารราชการได้ง่ายๆ เพียงไม่กี่คลิก
                       ประหยัดเวลาและเอกสารของคุณจะดูเป็นมืออาชีพมากขึ้น!✨</Typography>
                   </Grid>
                   <Grid item xs={12}>
@@ -41,7 +41,6 @@ export default function LoginPage() {
                       startIcon={<GoogleIcon />}
                       onClick={login}
                       fullWidth
-                      sx={style.buttonStyles}
                     >
                       Login with Google
                     </Button>
